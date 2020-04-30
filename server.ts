b08aa000468a9086a6c1d06f29a3fdf99fc069b1
Mark Waser <mark.waser@wisdom.digital>
2020-04-30 17:41:15 -0400

    Add project files.

---
+++ server.ts
@@ -0,0 +1,18 @@
+'use strict';
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config(); }
+var path = require('path');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var cors = require('cors');
+
+var app = express();
+app.use(cookieParser());
+app.use(cors());
+app.use(cors({ origin: "*", credentials: true }));
+
+var staticPath = path.join(__dirname, '/');
+app.use(express.static(staticPath));
+
+var srvr = app.listen(process.env.PORT || 1337, function () {
+    console.log('GBA GBBP API listening');
+});
